95090b2958d9f1247c91858b94e0e909fc2ada73
crwohlers <101900065+crwohlers@users.noreply.github.com>
2023-11-08 13:15:46 -0500

    property group -> ComponentInstance

---
+++ Shared/ComponentInstance.cs
@@ -0,0 +1,25 @@
+﻿using MinAVR.ComponentNamespace;
+
+namespace MinAVR.Shared
+{
+	public class ComponentInstance
+	{
+		public ComponentData data;
+
+		public int globalId;
+		public double x;
+
+		public double y;
+
+		public object mainValue;
+
+		public ComponentInstance(int globalId, object mainValueInitial, double x = 10.0, double y = 10.0) {
+			this.globalId = globalId;
+			data = ComponentDeclarations.components[globalId];
+			this.x = x;
+			this.y = y;
+			mainValue = mainValueInitial;
+
+		}
+	}
+}
