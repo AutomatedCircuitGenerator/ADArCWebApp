9886da5e67d180aa9777b7aab8b1f975fd57cc83
crwohlers <101900065+crwohlers@users.noreply.github.com>
2024-02-06 18:14:59 -0500

    initial testing for webworker multithreading
    implemented for dht22

---
+++ WebWorker.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics;
+
+namespace ADArCWebApp
+{
+
+	public interface IDelayService {
+		Task delay(int millisec);
+	}
+
+	public class DelayService : IDelayService
+	{
+		public async Task delay(int millisec)
+		{
+			var sw = new Stopwatch();
+			sw.Start();
+
+			while (sw.ElapsedMilliseconds > 0) {
+				await Task.Delay(1);
+			}
+
+			return;
+		}
+	}
+}
