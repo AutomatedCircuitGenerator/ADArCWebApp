d2af744867f9f63741ec3922735fb7d1606493f6
km5762 <kumyles@gmail.com>
2024-11-13 23:56:26 -0500

    forgot to add this in the last commit!

---
+++ wwwroot/lib/boards/board.ts
@@ -0,0 +1,72 @@
+﻿import {AVRUSART, PinState} from "@lib/avr8js";
+import {Controller} from "@controllers/controller";
+import {I2CController} from "@lib/i2c-bus";
+import {SPIByteTransferCallback} from "@lib/avr8js/peripherals/spi";
+import {u8} from "@lib/avr8js/types";
+
+export type PinListener = (state: PinState) => void;
+export type ClockEvent = () => void;
+export type ByteTransmitListener = (value: u8) => void;
+
+export interface Board {
+    cpu: CPU;
+    timers: Timer[];
+    usarts: USART[];
+    twis: TWI[];
+    spis: SPI[];
+    pins: Interfaces[];
+}
+
+export type Interfaces = {
+    digital?: Digital,
+    analog?: Analog,
+    usart?: USART,
+    spi?: SPI,
+    twi?: TWI,
+    timer?: Timer,
+}
+
+export interface BoardConstructor {
+    new(program: Uint16Array): Board;
+}
+
+export interface CPU {
+    get frequency(): number;
+    get cycles(): number;
+    clock: () => void;
+    addClockEvent: (callback: ClockEvent, cycles: number) => void;
+}
+
+export interface Timer {
+}
+
+export interface Digital {
+    getState(): PinState;
+    setState(state: boolean): void;
+    addListener(listener: PinListener): void;
+}
+
+export interface Analog {
+    getVoltage(): number;
+    setVoltage(voltage: number): void;
+}
+
+export interface USART {
+    setOnByteTransmit: (listener: ByteTransmitListener) => void;
+}
+
+export interface TWI {
+    registerController(addr: number, device: I2CController): void;
+}
+
+export interface SPI {
+    addListener(listener:SPIByteTransferCallback): void;
+    removeListener(listener: SPIByteTransferCallback): void;
+    completeTransfer(receivedByte: number): void;
+    get transferCycles(): number;
+}
+
+export interface ADC {
+    get channelValues(): number[];
+}
+
