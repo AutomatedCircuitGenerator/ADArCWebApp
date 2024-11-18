780fd7453602d31f2c0e570545a2422caf3e8588
km5762 <kumyles@gmail.com>
2024-11-18 17:25:55 -0500

    Preventing dragging components before board is selected

--- Shared/BoardService.cs
+++ Shared/BoardService.cs
@@ -8,18 +8,19 @@ public enum Board
 {
     ArduinoUno,
     ArduinoMega,
+    None,
 }
 
 public class BoardService
 {
     private readonly IJSRuntime _js;
-    private Board _board = Board.ArduinoUno;
+    private Board _board = Board.None;
 
     // Constructor to inject IJSRuntime
     public BoardService(IJSRuntime js)
     {
         _js = js;
-    }       
+    }
 
     public Board Board
     {
@@ -32,6 +33,34 @@ public class BoardService
             }
 
             _board = value;
+        }
+    }
+
+    public string GetJson()
+    {
+        switch (_board)
+        {
+            case Board.ArduinoUno:
+                return
+                    "{\"LocalId\":1,\"Code\":\"\\n\\nchar outputPins[] = {};\\n\\n\\n\\n\\nvoid setup() {\\n  Serial.begin(9600);\\n  for (int i = 0; i \u003C 0; i\u002B\u002B) {\\n    pinMode(outputPins[i], OUTPUT);\\n  }\\n}\\n\\n\\nvoid loop() {\\n}\\n\",\"Comps\":{\"1\":{\"globalId\":1,\"x\":829,\"y\":219,\"zoomedX\":829,\"zoomedY\":219,\"midSignal\":0,\"counter\":0,\"Wbuffer\":0,\"RegAddr\":0,\"byteIndex\":0,\"timer\":0,\"compParams\":{}}}}";
+            case Board.ArduinoMega:
+                return
+                    "{\"LocalId\":1,\"Code\":\"\\n\\nchar outputPins[] = {};\\n\\n\\n\\n\\nvoid setup() {\\n  Serial.begin(9600);\\n  for (int i = 0; i \u003C 0; i\u002B\u002B) {\\n    pinMode(outputPins[i], OUTPUT);\\n  }\\n}\\n\\n\\nvoid loop() {\\n}\\n\",\"Comps\":{\"1\":{\"globalId\":2,\"x\":829,\"y\":219,\"zoomedX\":829,\"zoomedY\":219,\"midSignal\":0,\"counter\":0,\"Wbuffer\":0,\"RegAddr\":0,\"byteIndex\":0,\"timer\":0,\"compParams\":{}}}}";
+            default:
+                return "";
+        }
+    }
+
+    public ComponentInstance GetComponentInstance()
+    {
+        switch (_board)
+        {
+            case Board.ArduinoUno:
+                return new ComponentInstance(1, null, 829, 219);
+            case Board.ArduinoMega:
+                return new ComponentInstance(2, null, 829, 219);
+            default:
+                return null;
         }
     }
 }
