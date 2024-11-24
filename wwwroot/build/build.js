9caf40ce1aef75131f850f14a462f7977fd3db88
Sargeanthost <sargeanthost@gmail.com>
2024-11-24 02:53:04 -0500

    pwm works as designed, just not designed correctly. need to find way to do percentage of pulse, not percentage of all cycles. If you have a sketch that cycles through colors, youll eventually just get a gray.
    dc motor wasnt supposed to have that line uncommented

--- wwwroot/build/build.js
+++ wwwroot/build/build.js
@@ -13808,6 +13808,7 @@ define("controllers/rgbled", ["require", "exports", "controllers/controller", "l
             this.rIsFirstCall = true;
             this.gIsFirstCall = true;
             this.bIsFirstCall = true;
+            this.animationFrameId = null;
         }
         setup() {
             this.rLastState = avr8js_11.PinState.Input;
@@ -13831,9 +13832,19 @@ define("controllers/rgbled", ["require", "exports", "controllers/controller", "l
             this.rIsFirstCall = true;
             this.gIsFirstCall = true;
             this.bIsFirstCall = true;
+            this.animationFrameId = null;
             this.pins.R[0].digital.addListener(this.rListener.bind(this));
             this.pins.G[0].digital.addListener(this.gListener.bind(this));
             this.pins.B[0].digital.addListener(this.bListener.bind(this));
+        }
+        cleanup() {
+            if (this.animationFrameId) {
+                cancelAnimationFrame(this.animationFrameId);
+            }
+            this.rBrightness = 0;
+            this.gBrightness = 0;
+            this.bBrightness = 0;
+            this.renderSvg();
         }
         rListener(state) {
             if (this.rIsFirstCall) {
@@ -13857,6 +13868,9 @@ define("controllers/rgbled", ["require", "exports", "controllers/controller", "l
             }
             else {
                 this.rBrightness = Math.min(this.rHighCycles / (execute_13.AVRRunner.getInstance().board.cpu.cycles - this.rFirstCallCycles), 1);
+            }
+            if (!this.animationFrameId) {
+                this.animationFrameId = requestAnimationFrame(this.renderSvg.bind(this));
             }
         }
         gListener(state) {
@@ -13882,6 +13896,9 @@ define("controllers/rgbled", ["require", "exports", "controllers/controller", "l
             else {
                 this.gBrightness = Math.min(this.gHighCycles / (execute_13.AVRRunner.getInstance().board.cpu.cycles - this.gFirstCallCycles), 1);
             }
+            if (!this.animationFrameId) {
+                this.animationFrameId = requestAnimationFrame(this.renderSvg.bind(this));
+            }
         }
         bListener(state) {
             if (this.bIsFirstCall) {
@@ -13906,6 +13923,34 @@ define("controllers/rgbled", ["require", "exports", "controllers/controller", "l
             else {
                 this.bBrightness = Math.min(this.bHighCycles / (execute_13.AVRRunner.getInstance().board.cpu.cycles - this.bFirstCallCycles), 1);
             }
+            if (!this.animationFrameId) {
+                this.animationFrameId = requestAnimationFrame(this.renderSvg.bind(this));
+            }
+        }
+        renderSvg() {
+            const totalBrightness = Math.max(this.rBrightness, this.gBrightness, this.bBrightness);
+            const totalOpacity = totalBrightness != 0 ? 0.2 + totalBrightness * 0.6 : 0;
+            const redBlur = this.element.querySelector("#rgbRedBlur");
+            redBlur.setAttribute("stdDeviation", `${this.rBrightness * 3}`);
+            const greenBlur = this.element.querySelector("#rgbGreenBlur");
+            greenBlur.setAttribute("stdDeviation", `${this.gBrightness * 3}`);
+            const blueBlur = this.element.querySelector("#rgbBlueBlur");
+            blueBlur.setAttribute("stdDeviation", `${this.bBrightness * 3}`);
+            const redCircle = this.element.querySelector("#rgbRedCircle");
+            redCircle.setAttribute("r", `${this.rBrightness * 5 + 2}`);
+            redCircle.setAttribute("opacity", `${Math.min(this.rBrightness * 20, 0.3)}`);
+            const greenCircle = this.element.querySelector("#rgbGreenCircle");
+            greenCircle.setAttribute("r", `${this.gBrightness * 5 + 2}`);
+            greenCircle.setAttribute("opacity", `${Math.min(this.gBrightness * 20, 0.3)}`);
+            const blueCircle = this.element.querySelector("#rgbBlueCircle");
+            blueCircle.setAttribute("r", `${this.bBrightness * 5 + 2}`);
+            blueCircle.setAttribute("opacity", `${Math.min(this.bBrightness * 20, 0.3)}`);
+            const mixedCircle = this.element.querySelector("#rgbMixedCircle");
+            mixedCircle.setAttribute("fill", `rgb(${this.rBrightness * 255}, ${this.gBrightness * 255 + this.bBrightness * 90}, ${this.bBrightness * 255})`);
+            mixedCircle.setAttribute("opacity", `${totalOpacity}`);
+            const hollowCircle = this.element.querySelector("#rgbHollowCircle");
+            hollowCircle.setAttribute("opacity", `${totalOpacity}`);
+            this.animationFrameId = null;
         }
     }
     exports.RGBLED = RGBLED;
--- wwwroot/controllers/rgbled.ts
+++ wwwroot/controllers/rgbled.ts
@@ -29,6 +29,7 @@ export class RGBLED extends Controller {
     private gIsFirstCall: boolean = true;
     private bIsFirstCall: boolean = true;
 
+    private animationFrameId: number | null = null;
 
     setup() {
         this.rLastState = PinState.Input;
@@ -59,9 +60,25 @@ export class RGBLED extends Controller {
         this.gIsFirstCall = true;
         this.bIsFirstCall = true;
 
+        this.animationFrameId = null;
+
+        // if (this.animationFrameId) {
+        //     cancelAnimationFrame(this.animationFrameId);
+        // }
+
         this.pins.R[0].digital.addListener(this.rListener.bind(this));
         this.pins.G[0].digital.addListener(this.gListener.bind(this));
         this.pins.B[0].digital.addListener(this.bListener.bind(this));
+    }
+
+    cleanup() {
+        if (this.animationFrameId) {
+            cancelAnimationFrame(this.animationFrameId);
+        }
+        this.rBrightness = 0;
+        this.gBrightness = 0;
+        this.bBrightness = 0;
+        this.renderSvg();
     }
 
     rListener(state: PinState) {
@@ -90,7 +107,12 @@ export class RGBLED extends Controller {
         } else {
             this.rBrightness = Math.min(this.rHighCycles / (AVRRunner.getInstance().board.cpu.cycles - this.rFirstCallCycles), 1);
         }
+
+        if (!this.animationFrameId) {
+            this.animationFrameId = requestAnimationFrame(this.renderSvg.bind(this));
+        }
     }
+
     gListener(state: PinState) {
         if (this.gIsFirstCall) {
             //We may be called at some point in the future when we havent started the pwm yet. still to be tested
@@ -117,7 +139,11 @@ export class RGBLED extends Controller {
         } else {
             this.gBrightness = Math.min(this.gHighCycles / (AVRRunner.getInstance().board.cpu.cycles - this.gFirstCallCycles), 1);
         }
+        if (!this.animationFrameId) {
+            this.animationFrameId = requestAnimationFrame(this.renderSvg.bind(this));
+        }
     }
+
     bListener(state: PinState) {
         if (this.bIsFirstCall) {
             //We may be called at some point in the future when we havent started the pwm yet. still to be tested
@@ -144,5 +170,41 @@ export class RGBLED extends Controller {
         } else {
             this.bBrightness = Math.min(this.bHighCycles / (AVRRunner.getInstance().board.cpu.cycles - this.bFirstCallCycles), 1);
         }
+        if (!this.animationFrameId) {
+            this.animationFrameId = requestAnimationFrame(this.renderSvg.bind(this));
+        }
+    }
+
+    private renderSvg() {
+        const totalBrightness = Math.max(this.rBrightness, this.gBrightness, this.bBrightness);
+        const totalOpacity = totalBrightness != 0 ? 0.2 + totalBrightness * 0.6 : 0;
+
+        const redBlur: Element = this.element.querySelector("#rgbRedBlur");
+        redBlur.setAttribute("stdDeviation", `${this.rBrightness * 3}`);
+        const greenBlur = this.element.querySelector("#rgbGreenBlur");
+        greenBlur.setAttribute("stdDeviation", `${this.gBrightness * 3}`);
+        const blueBlur = this.element.querySelector("#rgbBlueBlur");
+        blueBlur.setAttribute("stdDeviation", `${this.bBrightness * 3}`);
+
+        const redCircle = this.element.querySelector("#rgbRedCircle");
+        redCircle.setAttribute("r", `${this.rBrightness * 5 + 2}`);
+        redCircle.setAttribute("opacity", `${Math.min(this.rBrightness * 20, 0.3)}`);
+
+        const greenCircle = this.element.querySelector("#rgbGreenCircle");
+        greenCircle.setAttribute("r", `${this.gBrightness * 5 + 2}`);
+        greenCircle.setAttribute("opacity", `${Math.min(this.gBrightness * 20, 0.3)}`);
+
+        const blueCircle = this.element.querySelector("#rgbBlueCircle");
+        blueCircle.setAttribute("r", `${this.bBrightness * 5 + 2}`);
+        blueCircle.setAttribute("opacity", `${Math.min(this.bBrightness * 20, 0.3)}`);
+
+        const mixedCircle = this.element.querySelector("#rgbMixedCircle");
+        mixedCircle.setAttribute("fill", `rgb(${this.rBrightness * 255}, ${this.gBrightness * 255 + this.bBrightness * 90}, ${this.bBrightness * 255})`);
+        mixedCircle.setAttribute("opacity", `${totalOpacity}`);
+
+        const hollowCircle = this.element.querySelector("#rgbHollowCircle");
+        hollowCircle.setAttribute("opacity", `${totalOpacity}`);
+
+        this.animationFrameId = null;
     }
 }
