ca0974a5cd194c6f4b3d0e95c9b886e333f978e1
Bradley Kohler <bradleyk0205@gmail.com>
2025-10-03 14:38:31 -0400

    Add controller for pH sensor

---
+++ wwwroot/controllers/srvph.ts
@@ -0,0 +1,29 @@
+import {Controller} from "@controllers/controller";
+import {AVRRunner} from "@lib/execute";
+
+export class SRVPH extends Controller {
+    
+    private ph: number;
+
+    override update(state: Record<string, any>) {
+        this.setph(state.ph);
+    }
+
+    setph(ph: number) {
+        this.ph = ph
+        this.pins.ADC[0].analog.voltage = this.phToVoltage(this.ph);
+    }
+
+    setup() {
+        this.pins.ADC[0].analog.voltage = this.phToVoltage(this.ph);
+    }
+
+    /**
+     * Converts from ph to voltage, 
+     * following formula from https://files.atlas-scientific.com/Surveyor-pH-datasheet.pdf
+     * @param ph
+     */
+    phToVoltage(ph: number) {
+        return (ph - 15.509)/(-5.6548) 
+    }
+}
