49a3e869a71cce0e8719fc4c9f98b4c146caa8b8
SupItsSameer <156853348+SupItsSameer@users.noreply.github.com>
2025-10-17 16:06:01 -0400

    strain gauge code implementation

---
+++ wwwroot/controllers/straingauge.ts
@@ -0,0 +1,37 @@
+﻿import {Controller} from "@controllers/controller";
+import {Digital} from "../boards/board";
+import {PinState} from "@lib/avr8js";
+
+export class StrainGauge extends Controller {
+    private clock?: Digital;
+    private data?: Digital;
+    private weight = 100;
+    private bitIndex = 23;
+
+    override update(state: Record<string, any>) {
+        this.setWeight(state.weight);
+    }
+
+    setup(): void {
+        this.data = this.pins.dout[0].digital;
+        this.clock = this.pins.clk[0].digital;
+        this.bitIndex = 23;
+
+        this.clock.addListener((state: PinState) => this.handleClock(state))
+    }
+
+    setWeight(weight: number) {
+        this.weight = weight;
+    }
+    private handleClock(state: PinState) {
+        if (state === PinState.Low) {
+            this.data.state = ((this.weight >> this.bitIndex) & 1) === 1;
+            this.bitIndex--;
+
+            if (this.bitIndex < 0) {
+                this.bitIndex = 24;
+            }
+
+        }
+    }
+}
