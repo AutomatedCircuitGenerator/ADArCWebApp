4023377b99a74c26bee690927618c4d9571b2c5b
nj-anderson <norahjanderson@gmail.com>
2025-12-03 15:58:28 -0500

    Updated main.ts to add RELAY

--- wwwroot/controllers/relay.ts
+++ wwwroot/controllers/relay.ts
@@ -1,9 +1,7 @@
 import {Controller} from "./controller";
-import {AVRRunner} from "@lib/execute";
-import {PinState} from "@lib/avr8js"
 
 export class RELAY extends Controller {
     setup() {
-        AVRRunner.getInstance();
+
     }
 }
