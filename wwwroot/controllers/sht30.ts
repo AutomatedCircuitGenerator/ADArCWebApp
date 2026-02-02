4f145ccba6682eb623b8d9e4dc4662138751e9a7
cacsibal <cacsibal@gmail.com>
2026-02-02 15:19:09 -0500

    started sim code impl

--- wwwroot/controllers/sht30.ts
+++ wwwroot/controllers/sht30.ts
@@ -1,8 +1,25 @@
 import {Controller} from "./controller";
 
 export class SHT30 extends Controller {
+    private humidity: number; // relative humidity, [0, 1]
+    private temperature: number;
     
     setup() {
     }
-
+    
+    override update(state: Record<string, any>) {
+        this.setHumidity(state.humidity);
+        this.setTemperature(state.temperature);
+    }
+    
+    setHumidity(humidity: number) {
+    //     todo
+    }
+    
+    setTemperature(temperature: number) {
+        this.temperature = temperature;
+        
+    //     todo
+    }
+    
 }
