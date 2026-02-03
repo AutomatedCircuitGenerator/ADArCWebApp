482c807be1480d16e8732b49d676beac890a09bd
“Bradley <bradleyk0205@gmail.com>
2026-02-03 02:33:01 -0500

    Implement nema 17 stepper motor simulation without animation

--- wwwroot/controllers/nema17.ts
+++ wwwroot/controllers/nema17.ts
@@ -1,138 +1,42 @@
-import {Controller} from "@controllers/controller";
-import {PinState} from "@lib/avr8js";
-import {AVRRunner} from "@lib/execute";
-
-//https://cookierobotics.com/042/
-// When Orange is high, Pink is low. Orange is North
-// When Yellow is high, Blue is low. Yellow is North
-enum ProngState {
-    Low = 0,
-    High = 1,
-}
-
-interface Prong {
-    state: ProngState;
-}
-
-enum Coil {
-    A = 0,
-    B = 1,
-    C = 2,
-    D = 3
-}
-
-class StepperMotor {
-    // Prongs are 
-    // Initialize 32 prongs, all set to Low initially. Note that one is offset into the next rotation
-    prongs: Prong[] = Array.from({length: 32}, () => ({
-        state: ProngState.Low,
-    }));
-    private prongAngles: number[] = Array.from([this.prongs.length], (_, index) => index * 11.40625)
-
-    headAngle: number;
-
-    //Make sure to call for every single coil on every single 
-    setCoilState(coil: Coil, state: ProngState) {
-        for (let step = coil; step < this.prongs.length; step += 4) {
-            this.prongs[step].state = state;
-        }
-        this.adjustHead();
-    }
-
-    getCoilState(coil: Coil) {
-        return this.prongs[coil].state.valueOf()
-    }
-
-    private adjustHead() {
-        // at and in between two prongs is a valid state, giving us 365/64 = 5.703125 degree increments to move our 
-        // pointing. Not worrying about microstepping since this is uln2003 driver
-
-        //if there are >= three active we can just skip adjusting, were at an in between step
-        if (this.prongs.filter(prong => prong.state === ProngState.High).length < 16) {
-            return;
-        }
-
-    }
-}
+import { Controller } from "@controllers/controller";
+import { PinState } from "@lib/avr8js";
+import { Digital } from "../boards/board";
 
 export class NEMA17 extends Controller {
-    // each step is 11.25deg in full step mode
-    // 5.625 in half step
+    private stepPin!: Digital;
+    private dirPin!: Digital;
+    private currentPos: number;
+    private stepsPerRev: number;
 
-    // The degree the north part of the rotor is facing. starts at 0, 
-    // private motor = new StepperMotor();
-
-    private animationFrameId: number | null = null;
-
+    override update(state: Record<string, any>) {
+        this.stepsPerRev = Math.max(0, state.stepsPerRev || 200);
+    }
+    
     setup() {
-        // this.angle = 0;
-        this.pins.in1[0].digital.addListener((state) => this.aListener(state));
-        this.pins.in2[0].digital.addListener((state) => this.bListener(state));
-        this.pins.in3[0].digital.addListener((state) => this.cListener(state));
-        this.pins.in4[0].digital.addListener((state) => this.dListener(state));
-
-        // console.log(`pin index 1: ${this.pinIndices.in1[0].valueOf()}`);
-        // console.log(`pin index 2: ${this.pinIndices.in2[0].valueOf()}`);
-        // console.log(`pin index 3: ${this.pinIndices.in3[0].valueOf()}`);
-        // console.log(`pin index 4: ${this.pinIndices.in4[0].valueOf()}`);
-
-        this.animationFrameId = null;
+        this.stepPin = this.pins.STEP[0].digital;
+        this.dirPin = this.pins.DIR[0].digital;
+        this.currentPos = 0;
+        this.stepsPerRev = 200;
+        
+        // Every falling edge on STEP triggers a step
+        this.stepPin.addListener(this.onStep.bind(this));
     }
 
     cleanup() {
-        // this.angle = 0;
-        // renderSvg();
-        const animationElem: SVGAnimationElement = this.element.querySelector("#_28byj-shaft-rotateAnim")
-        const svgElem: SVGSVGElement = this.element.querySelector("#_28byj");
-        animationElem.endElement()
-        svgElem.pauseAnimations()
+        // nothing to cleanup for console output
     }
 
+    private onStep(state: PinState) {
+        if (state !== PinState.Low) return; // only act on falling edge
 
-    private randWaitAmount = 16000000; //1 second
-    private lastSpinState: boolean = false;
-    private first: boolean = true;
-    private wasPrevFirst: boolean = false
+        const dir = this.dirPin.state === PinState.High ? 1 : -1;
+        this.currentPos += dir;
 
-    private setSpinning(isSpinning: boolean) {
-        const animationElem: SVGAnimationElement = this.element.querySelector("#_28byj-shaft-rotateAnim")
-        const svgElem: SVGSVGElement = this.element.querySelector("#_28byj");
-        if (this.first) {
-            animationElem.beginElement()
-            this.first = false;
-            this.wasPrevFirst = true;
-        } else {
-            if (!this.wasPrevFirst && isSpinning == this.lastSpinState) {
-                //if the previous call was the first call, wasprev will be false and break out
-                return
-            }
-            this.wasPrevFirst = false;
-            if (isSpinning) {
-                svgElem.unpauseAnimations()
-                //the code simply doesnt call us for some time, which signals we should stop. no way to handle thise except calling the functino again with false
-                AVRRunner.getInstance().board.cpu.addClockEvent(() => {
-                    this.setSpinning(false)
-                }, this.randWaitAmount);
-            } else {
-                svgElem.pauseAnimations()
-            }
-        }
-        this.lastSpinState = isSpinning
-    }
+        const angle = (this.currentPos % this.stepsPerRev) * (360 / this.stepsPerRev);
+        const directionStr = dir === 1 ? "Clockwise" : "Counter-Clockwise";
 
-    aListener(state: PinState) {
-        this.setSpinning(true)
-    }
-
-    bListener(state: PinState) {
-        this.setSpinning(true)
-    }
-
-    cListener(state: PinState) {
-        this.setSpinning(true)
-    }
-
-    dListener(state: PinState) {
-        this.setSpinning(true)
+        console.log(
+            `Step: ${this.currentPos}, Angle: ${angle.toFixed(2)}°, Direction: ${directionStr}`
+        );
     }
 }
