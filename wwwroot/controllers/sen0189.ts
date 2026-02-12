2dc84e744a557c82e632928345daff7d7c036da0
nj-anderson <norahjanderson@gmail.com>
2026-02-12 13:49:23 -0500

    Sim code implemented

--- wwwroot/controllers/sen0189.ts
+++ wwwroot/controllers/sen0189.ts
@@ -1,9 +1,49 @@
 import {Controller} from "./controller";
-import {AVRRunner} from "@lib/execute";
-//import {PinState} from "@lib/avr8js";
-
 export class SEN0189 extends Controller {
+    
+    private turbidity: number = 0;
+    private inSimulation: boolean;
+
+    override update(state: Record<string, any>) {
+        this.setturbidity(state.turbidity);
+    }
+    
+    setturbidity(turbidity: number) {
+        
+        // Clamp turbidity, sensor's range is only 0-3000
+        if (turbidity < 0)
+            this.turbidity = 0;
+        else if (turbidity > 3000)
+            this.turbidity = 3000;
+        else
+            this.turbidity = turbidity;
+
+        if (!this.inSimulation) {
+            return;
+        }
+
+        this.pins.ADC[0].analog.voltage = this.turbidityToVoltage(this.turbidity);
+    }
+
     setup() {
-        AVRRunner.getInstance();
+        this.inSimulation = true;
+        this.pins.ADC[0].analog.voltage = this.turbidityToVoltage(this.turbidity);
+    }
+    
+    turbidityToVoltage(turbidity: number){
+        let discriminant = (5742.3 * 5742.3) -
+            (4 * -1120.4 * (-(4352.9 + turbidity)));
+
+        if (discriminant < 0) {
+            return 0;
+        }
+
+        let sqrtDisc = Math.sqrt(discriminant);
+
+        let v1 = (-5742.3 + sqrtDisc) / (2 * -1120.4);
+        let v2 = (-5742.3 - sqrtDisc) / (2 * -1120.4);
+
+        // choose the voltage in a valid range (adjust if needed)
+        return (v1 >= 0 && v1 <= 5) ? v1 : v2;
     }
 }
