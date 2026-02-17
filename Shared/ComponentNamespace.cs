a361a574489fc6c3d42891464251638875a436cc
SupItsSameer <156853348+SupItsSameer@users.noreply.github.com>
2026-02-17 13:19:43 -0500

    Arduino Nano Base

--- Shared/ComponentNamespace.cs
+++ Shared/ComponentNamespace.cs
@@ -706,6 +706,20 @@ namespace ADArCWebApp.Shared
                             { "functions", "" }, { "delayLoop", "" }, { "delayTime", "" }
                         }, paneHoverText: "SEN0114", pins: ["gnd", "5V", "analog_out"], gsNodeName: "sen0114")
                     .Property("humidity", 512.0).Finish()
+            },
+            {
+                1,
+                new ComponentDataBuilder("Nano", false, "Arduino", 0.33, 280, 210, typeof(RazorArduinoNano),
+                    codeForGen: new()
+                    {
+                        { "include", "" }, { "global", "" }, { "setup", "" }, { "loopMain", "" }, { "functions", "" },
+                        { "delayLoop", "" }, { "delayTime", "" }
+                    },
+                    pins:
+                    [
+                        "d0", "d1", "d2", "d3", "d4", "d5", "d6", "d7", "d8", "d9", "d10", "d11", "d12", "d13", "d14",
+                        "d15", "d16", "d17", "d18", "d19", "a0", "a1", "a2", "a3", "a4", "a5", "a6", "a7", "gnd", "5V", "3V3"
+                    ], gsNodeName: "arduinoUnoRev3").Finish()
             }
         };
     }
