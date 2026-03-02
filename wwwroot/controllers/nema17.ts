48a90d5346d7296e1609dde54106ad2bd13a0c99
“Bradley <bradleyk0205@gmail.com>
2026-03-01 23:35:31 -0500

    Implement rotor animation

--- wwwroot/controllers/nema17.ts
+++ wwwroot/controllers/nema17.ts
@@ -3,10 +3,14 @@ import { PinState } from "@lib/avr8js";
 import { Digital } from "../boards/board";
 
 export class NEMA17 extends Controller {
-    private stepPin!: Digital;
-    private dirPin!: Digital;
+    private stepPin: Digital;
+    private dirPin: Digital;
+    
     private currentPos: number;
     private stepsPerRev: number;
+    
+    private motorShaft: SVGGElement | null;
+    private originalTransform: string;
 
     override update(state: Record<string, any>) {
         this.stepsPerRev = Math.max(0, state.stepsperrev || 200);
@@ -15,28 +19,45 @@ export class NEMA17 extends Controller {
     setup() {
         this.stepPin = this.pins.STEP[0].digital;
         this.dirPin = this.pins.DIR[0].digital;
+        
         this.currentPos = 0;
         this.stepsPerRev = 200;
+        this.motorShaft = this.element.querySelector("#g22") as SVGGElement | null;
+        this.originalTransform = this.motorShaft?.getAttribute("transform") || "";
+        if (!this.motorShaft)
+            console.error("Motor shaft not found in SVG. Animation will be disabled. Check your SVG file. ");
         
         // Every falling edge on STEP triggers a step
         this.stepPin.addListener(this.onStep.bind(this));
-    }
-
-    cleanup() {
-        // nothing to cleanup for console output
+        
+        this.onStep(PinState.Low);
     }
 
     private onStep(state: PinState) {
         if (state !== PinState.Low) return; // only act on falling edge
+        
+        if (!this.motorShaft) return;
+        
+        const direction = this.dirPin.state === PinState.High ? 1 : -1;
+        
+        this.currentPos += direction;
+        this.currentPos %= this.stepsPerRev;
+        
+        // Keep motor position positive
+        if (this.currentPos < 0) 
+            this.currentPos += this.stepsPerRev;
+        const angle = (this.currentPos / this.stepsPerRev) * 2 * Math.PI;
 
-        const dir = this.dirPin.state === PinState.High ? 1 : -1;
-        this.currentPos += dir;
-
-        const angle = (this.currentPos % this.stepsPerRev) * (360 / this.stepsPerRev);
-        const directionStr = dir === 1 ? "Clockwise" : "Counter-Clockwise";
-
-        console.log(
-            `Step: ${this.currentPos}, Angle: ${angle.toFixed(2)}°, Direction: ${directionStr}`
+        const radius = 0.2;
+        
+        const dx = radius * Math.cos(angle) * Math.cos(Math.PI / 4); // Scale the x displacement because the shaft is at an angle to the viewbox
+        const dy = radius * Math.sin(angle);
+        
+        const newTransform = `translate(${dx} ${dy}) ${this.originalTransform}`;
+        
+        this.motorShaft?.setAttribute(
+            "transform",
+            newTransform
         );
     }
 }
