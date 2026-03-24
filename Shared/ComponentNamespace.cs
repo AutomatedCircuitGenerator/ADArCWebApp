5b678505ce8c92ea1555ff17804e96445f51c730
Anand P <anandp@Anands-MacBook-Air.local>
2026-03-24 16:42:40 -0400

    fully working needs minor fixes - svg and residual pulse

--- Shared/ComponentNamespace.cs
+++ Shared/ComponentNamespace.cs
@@ -709,51 +709,53 @@ namespace ADArCWebApp.Shared
                     }, pins: ["Vcc", "gnd", "ADC"], gsNodeName: "srv-ph").Property("ph", 7.0).Finish()
             },
             {
-                35,
-                new ComponentDataBuilder("Geiger counter", true, "Input/Other Sensors", 1, 18.5,
-                    19.154, typeof(RazorJ305B), paneHoverText: "J305B",
-                    codeForGen: new()
-                    {
+                39,
+                new ComponentDataBuilder("Geiger Counter", true, "Input/Other Sensors", 1, 75, 75,
+                        typeof(RazorJ305B), paneHoverText: "J305B",
+                        codeForGen: new()
                         {
-                            "include",
-                            "#include <Arduino.h>"
+                            {
+                                "include",
+                                "#include <Arduino.h>"
+                            },
+                            {
+                                "global",
+                                "volatile unsigned long geigerCount@ = 0;\n" +
+                                "unsigned long geigerLastTime@ = 0;\n" +
+                                "unsigned int cpm@ = 0;\n" +
+                                "int geigerPin@ = ~\"vin\";"
+                            },
+                            {
+                                "setup",
+                                "  pinMode(geigerPin@, INPUT);\n" +
+                                "  attachInterrupt(digitalPinToInterrupt(geigerPin@), geigerISR@, RISING);\n" +
+                                "  geigerLastTime@ = millis();"
+                            },
+                            {
+                                "loopMain",
+                                "  static unsigned long lastPrint@ = 0;\n" +
+                                "  if (millis() - lastPrint@ >= 2000) {\n" +
+                                "    lastPrint@ = millis();\n" +
+                                "    cpm@ = geigerCount@ * 30;\n" +
+                                "    Serial.print(\"CPM: \");\n" +
+                                "    Serial.println(cpm@);\n" +
+                                "    geigerCount@ = 0;\n" +
+                                "  }"
+                            },
+                            {
+                                "functions",
+                                "void geigerISR@() {\n" +
+                                "  geigerCount@++;\n" +
+                                "}"
+                            },
+                            { "delayLoop", "" },
+                            { "delayTime", "" }
                         },
-                        {
-                            "global",
-                            "volatile unsigned long geigerCount@ = 0;\n" +
-                            "unsigned long geigerLastTime@ = 0;\n" +
-                            "unsigned int cpm@ = 0;\n" +
-                            "int geigerPin@ = @;"
-                        },
-                        {
-                            "setup",
-                            "  Serial.begin(9600);\n" +
-                            "  pinMode(geigerPin@, INPUT);\n" +
-                            "  attachInterrupt(digitalPinToInterrupt(geigerPin@), geigerISR@, RISING);\n" +
-                            "  geigerLastTime@ = millis();"
-                         
-                        },
-                        {
-                            "loopMain",
-                            "  if (millis() - geigerLastTime@ >= 2000) {\n" +
-                            "      cpm@ = geigerCount@ * 30;\n" +
-                            "      geigerCount@ = 0;\n" +
-                            "      geigerLastTime@ = millis();\n" +
-                            "      Serial.print(\"CPM: \");\n" +
-                            "      Serial.println(cpm@);\n" +
-                            "  }"
-                        },
-                        {
-                            "functions",
-                            "void geigerISR@() {\n" +
-                            "  geigerCount@++;\n" +
-                            "}"
-                        },
-                        { "delayLoop", "" },
-                        { "delayTime", "" }
-                    }, pins: ["Vcc", "gnd", "vin"], gsNodeName: "j305b").Property("cpm", 15.0).Finish()
-                 }
-                
+                        pins: ["Vcc", "gnd", "vin"],
+                        gsNodeName: "j305b")
+                    .Property("cpm", 15.0)
+                    .Finish()
+            }
         };
     }
 }
--- wwwroot/controllers/j305b.ts
+++ wwwroot/controllers/j305b.ts
@@ -4,18 +4,24 @@ import { PinState } from "@lib/avr8js";
 
 export class J305B extends Controller {
 
-    private _cpm: number = 10;          // counts per minute
-    private pulseIntervalCycles = 0;   // CPU cycles between pulses
+    private _cpm: number = 15;
+    private pulseIntervalCycles = 0;
+    private lastPulseTime = 0;
 
     override update(state: Record<string, any>) {
         if (state.cpm !== undefined) {
-            this._cpm = state.cpm;
+            this._cpm = Math.max(0, state.cpm);
+            console.log("[J305B] CPM updated to:", this._cpm);
             this.recalculateInterval();
+            // Force next pulse to be scheduled fresh
+            this.lastPulseTime = AVRRunner.getInstance().board.cpu.cycles;
         }
     }
 
     setup() {
+        console.log("[J305B] Setup complete, CPM:", this._cpm);
         this.recalculateInterval();
+        this.lastPulseTime = AVRRunner.getInstance().board.cpu.cycles;
         this.scheduleNextPulse();
     }
 
@@ -27,18 +33,27 @@ export class J305B extends Controller {
 
         const pulsesPerSecond = this._cpm / 60.0;
         const secondsPerPulse = 1.0 / pulsesPerSecond;
-
-        this.pulseIntervalCycles =
-            Math.floor(secondsPerPulse * AVRRunner.getInstance().board.cpu.frequency);
+        this.pulseIntervalCycles = Math.floor(secondsPerPulse * AVRRunner.getInstance().board.cpu.frequency);
     }
 
     private scheduleNextPulse() {
         if (this.pulseIntervalCycles <= 0) {
+            console.log("[J305B] CPM is 0, no pulses scheduled");
             return;
         }
 
         AVRRunner.getInstance().board.cpu.addClockEvent(() => {
+            // Check if CPM changed and reschedule
+            const currentCycles = AVRRunner.getInstance().board.cpu.cycles;
+            if (currentCycles - this.lastPulseTime > this.pulseIntervalCycles * 1.5) {
+                // CPM was updated, restart scheduling
+                this.lastPulseTime = currentCycles;
+                this.scheduleNextPulse();
+                return;
+            }
+
             this.emitPulse();
+            this.lastPulseTime = AVRRunner.getInstance().board.cpu.cycles;
             this.scheduleNextPulse();
         }, this.pulseIntervalCycles);
     }
@@ -46,7 +61,7 @@ export class J305B extends Controller {
     private emitPulse() {
         const vin = this.pins.vin[0].digital;
 
-        // Ensure LOW first
+        // Send RISING pulse for interrupt detection
         vin.state = false;
 
         AVRRunner.getInstance().board.cpu.addClockEvent(() => {
@@ -54,7 +69,7 @@ export class J305B extends Controller {
 
             AVRRunner.getInstance().board.cpu.addClockEvent(() => {
                 vin.state = false;
-            }, 200); // slightly longer pulse
+            }, 100);
         }, 50);
     }
 }
