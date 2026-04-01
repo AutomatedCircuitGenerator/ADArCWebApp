a6df2600f8613b6fb306f6c12c3a355294d8d115
Anand P <anandp@autoreg-4620792.dyn.wpi.edu>
2026-04-01 05:47:54 -0400

    pin state fixes

--- wwwroot/controllers/transceiver.ts
+++ wwwroot/controllers/transceiver.ts
@@ -1,25 +1,27 @@
 import { Controller } from "./controller";
+import { Digital } from "../boards/board";
+import { PinState } from "@lib/avr8js";
 
 export class TRANSCEIVER extends Controller {
-
     private mode: number = 0; // 0 = receive, 1 = transmit
 
     setup() {
         console.log("[Transceiver] Setup complete - Mode: receive");
 
         // Initialize pin state
-        const csnPin = this.pins.csn[0].digital;
-        csnPin.state = false; // default = receive
+        const csnPin = this.pins.csn[0].digital as Digital;
+
+        // Use the correct PinState enum/object
+        csnPin.state = PinState.Low; // default = receive
     }
 
-    // ✅ THIS FIXES YOUR ERROR
     setMode(modeValue: number) {
         this.mode = modeValue;
 
-        const csnPin = this.pins.csn[0].digital;
+        const csnPin = this.pins.csn[0].digital as Digital;
 
         // Controller OWNS csn pin
-        csnPin.state = (modeValue === 1); // HIGH = transmit
+        csnPin.state = modeValue === 1 ? PinState.High : PinState.Low; // HIGH = transmit
 
         console.log(
             "[Transceiver] Mode changed to:",
