67f477fd880a47d3ed83f0fb708d4eee3a986a66
Stephen He <stephenhe2023@gmail.com>
2026-06-24 21:19:05 -0400

    tried to present text with pixel

--- wwwroot/controllers/ssd1306.ts
+++ wwwroot/controllers/ssd1306.ts
@@ -3,17 +3,25 @@ import { Controller } from "@controllers/controller";
 
 export class SSD1306 extends Controller implements I2CController {
 
-    private updated = true;
     private displayText = "SSD1306";
+    private updated = true;
+    private buffer = new Uint8Array(128 * 64);
 
     setup() {
         console.log("SSD1306 setup");
-        console.log("registering at", 0x3C);
+
         this.pins.scl[0].twi.registerController(0x3C, this);
+        
+        for(let x=0;x<128;x++){
+            this.buffer[32 * 128 + x] = 1;
+        }
+
         this.render();
     }
 
-    cleanup() {}
+    cleanup() {
+        console.log("SSD1306 cleanup");
+    }
 
     update() {
         if (this.updated) {
@@ -23,14 +31,45 @@ export class SSD1306 extends Controller implements I2CController {
     }
 
     render() {
-        console.log("displayText =", this.displayText);
-        const text = this.element.querySelector("#oledText");
-        console.log("text =", text);
 
-        if (text) {
-            text.textContent = this.displayText;
+        const pixelGroup =
+            this.element.querySelector("#oledPixels");
+
+        if (!pixelGroup) {
+            return;
         }
-        console.log("render");
+
+        pixelGroup.innerHTML = "";
+
+        const startX = 612;
+        const startY = 350;
+
+        const pixelWidth = 386 / 128;
+        const pixelHeight = 197 / 64;
+
+        for (let y = 0; y < 64; y++) {
+
+            for (let x = 0; x < 128; x++) {
+
+                if (!this.buffer[y * 128 + x]) {
+                    continue;
+                }
+
+                const rect = document.createElementNS("http://www.w3.org/2000/svg", "rect");
+
+                rect.setAttribute("x", (startX + x * pixelWidth).toString());
+
+                rect.setAttribute("y", (startY + y * pixelHeight).toString());
+
+                rect.setAttribute("width", pixelWidth.toString());
+
+                rect.setAttribute("height", pixelHeight.toString());
+
+                rect.setAttribute("fill", "#88aaff");
+
+                pixelGroup.appendChild(rect);
+            }
+        }
     }
 
     i2cConnect() {
@@ -38,13 +77,16 @@ export class SSD1306 extends Controller implements I2CController {
         return true;
     }
 
-    i2cDisconnect() {}
+    i2cDisconnect() {
+        console.log("I2C DISCONNECT");
+    }
 
     i2cReadByte(): number {
-        return 0xff;
+        return 0xFF;
     }
 
     i2cWriteByte(value: number) {
+
         console.log("SSD1306 I2C:", value.toString(16));
 
         this.displayText = value.toString(16);
